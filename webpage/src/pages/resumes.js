75f021fbfca09f87cbd1ba25b79abf3a2debad26
Andy <Zhenghaifeng27@gmail.com>
2023-12-06 01:20:28 -0500

    display backend with front end

--- webpage/src/pages/resumes.js
+++ webpage/src/pages/resumes.js
@@ -1,9 +1,28 @@
-const Resume = () => {
+import React from 'react';
+import { useState, useEffect } from 'react';
+
+export default function Resume() {
+
+    const [resume, setResume] = useState({});
+
+    useEffect(() => {
+        fetch("/create-resume-test").then(
+            response => response.json([{}])
+        ).then(
+            data => {
+                setResume(data)
+            }
+        )
+    }, [])
+
+    console.log(resume.messages)
+
     return(
         <div>
-            Resume
+            <h1>Resume</h1>
+            <p> {resume.messages} </p>
         </div>
     )
+
 }
 
-export default Resume;
