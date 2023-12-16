cf3a3a574ce102a3cc5f945ed93cf010901d9132
Andy <zhenghaifeng27@gmail.com>
2023-12-15 23:15:39 -0500

    commit .gitignore and formatted backend

--- backend/openAI.js
+++ backend/openAI.js
@@ -1,6 +1,6 @@
-const express = require('express');
+const express = require("express");
 require("dotenv").config();
-const {OpenAI} = require('openai');
+const { OpenAI } = require("openai");
 const axios = require("axios");
 
 const app = express();
@@ -8,57 +8,54 @@ const app = express();
 app.use(express.json());
 
 const openai = new OpenAI({
-    apiKey: process.env.OPEN_AI_KEY,
+  apiKey: process.env.OPEN_AI_KEY,
 });
 
 let postDataResult = null;
 
-async function postData(text)
-{
-    console.log("testing")
+async function postData(text) {
+  console.log("testing");
 
-    const data = [
-        {
-            message: text
-        }
-    ]
+  const data = [
+    {
+      message: text,
+    },
+  ];
 
-    postDataResult = data;
+  postDataResult = data;
 }
 app.post("/create-resume", async (req, res) => {
-    try{
-        const response = await openai.chat.completions.create({
-            messages: [{ role: "system", content: "You are a helpful assistant." }],
-            model: "ada",
-          });
-        await postData("success")
-        return res.status(200).json({
-            success: true,
-        });
-    } catch (error){
-        await postData("fail")
-        return res.status(200).json({
-            success: postDataResult[0].message,
-            error: error.response ? error.response.data : error.message
-        })
-    }
+  try {
+    const response = await openai.chat.completions.create({
+      messages: [{ role: "system", content: "You are a helpful assistant." }],
+      model: "ada",
+    });
+    await postData("success");
+    return res.status(200).json({
+      success: true,
+    });
+  } catch (error) {
+    await postData("fail");
+    return res.status(200).json({
+      success: postDataResult[0].message,
+      error: error.response ? error.response.data : error.message,
+    });
+  }
 });
 
-app.get("/create-resume-test", (req,res) => 
-{
-    res.status(200).json({
-        message: postDataResult[0].message,
-    });
-})
+app.get("/create-resume-test", (req, res) => {
+  res.status(200).json({
+    message: postDataResult[0].message,
+  });
+});
 
-app.post("/send_test", async(req,res) => {
+app.post("/send_test", async (req, res) => {
+  const message = req.body;
+  console.log(message);
 
-    const message = req.body;
-    console.log(message)
-
-    return res.status(200).json();
-})
+  return res.status(200).json();
+});
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`server listening on port ${port}`))
+app.listen(port, () => console.log(`server listening on port ${port}`));
