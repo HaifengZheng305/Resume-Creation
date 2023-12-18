e7f7685f72c993c53bf9d365731add13242401c8
Andy <zhenghaifeng27@gmail.com>
2023-12-18 00:03:56 -0500

    created outline to create prompt and changed the ideal structure for resume

--- backend/openAI.js
+++ backend/openAI.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 require("dotenv").config();
 const { OpenAI } = require("openai");
 const axios = require("axios");
+const createPrompt = require("./essential_features/createPrompt");
 
 const app = express();
 
@@ -33,16 +34,18 @@ async function postData(text) {
 /*
 - test template in the format of my resume object;
 - convert the template into text or some way to be entered in to the content section of the nessage
-- convert the message back to the format of the original object
+- run test run into chat GPT;
 - include the api to input into a desire template
 - get the template back to the resume page as a template
 
 */
 app.post("/create-resume", async (req, res) => {
   const resume = req.body;
+  const prompt = createPrompt(resume);
+
   try {
     const response = await openai.chat.completions.create({
-      messages: [{ role: "system", content: "You are a helpful assistant." }],
+      messages: [{ role: "system", content: `${prompt}` }],
       model: "ada",
     });
     await postData("success");
